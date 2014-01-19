5f723d6cc94683db4e21586984683c48b30cc53e
Eric Hanchrow <eric.hanchrow@gmail.com>
2014-01-19 12:53:23 -0800

    Mollify flake8 by fiddling whitespace

--- pox/misc/of_tutorial.py
+++ pox/misc/of_tutorial.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#           http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,102 +29,97 @@ log = core.getLogger()
 
 
 class Tutorial (object):
-  """
-  A Tutorial object is created for each switch that connects.
-  A Connection object for that switch is passed to the __init__ function.
-  """
-  def __init__ (self, connection):
-    # Keep track of the connection to the switch so that we can
-    # send it messages!
-    self.connection = connection
-
-    # This binds our PacketIn event listener
-    connection.addListeners(self)
-
-    # Use this table to keep track of which ethernet address is on
-    # which switch port (keys are MACs, values are ports).
-    self.mac_to_port = {}
-
-
-  def resend_packet (self, packet_in, out_port):
     """
-    Instructs the switch to resend a packet that it had sent to us.
-    "packet_in" is the ofp_packet_in object the switch had sent to the
-    controller due to a table-miss.
+    A Tutorial object is created for each switch that connects.
+    A Connection object for that switch is passed to the __init__ function.
     """
-    msg = of.ofp_packet_out()
-    msg.data = packet_in
+    def __init__ (self, connection):
+        # Keep track of the connection to the switch so that we can
+        # send it messages!
+        self.connection = connection
 
-    # Add an action to send to the specified port
-    action = of.ofp_action_output(port = out_port)
-    msg.actions.append(action)
+        # This binds our PacketIn event listener
+        connection.addListeners(self)
 
-    # Send message to switch
-    self.connection.send(msg)
+        # Use this table to keep track of which ethernet address is on
+        # which switch port (keys are MACs, values are ports).
+        self.mac_to_port = {}
 
+    def resend_packet (self, packet_in, out_port):
+        """
+        Instructs the switch to resend a packet that it had sent to us.
+        "packet_in" is the ofp_packet_in object the switch had sent to the
+        controller due to a table-miss.
+        """
+        msg = of.ofp_packet_out()
+        msg.data = packet_in
 
-  def act_like_hub (self, packet, packet_in):
-    """
-    Implement hub-like behavior -- send all packets to all ports besides
-    the input port.
-    """
+        # Add an action to send to the specified port
+        action = of.ofp_action_output(port = out_port)
+        msg.actions.append(action)
 
-    # We want to output to all ports -- we do that using the special
-    # OFPP_ALL port as the output port.  (We could have also used
-    # OFPP_FLOOD.)
-    self.resend_packet(packet_in, of.OFPP_ALL)
+        # Send message to switch
+        self.connection.send(msg)
 
-    # Note that if we didn't get a valid buffer_id, a slightly better
-    # implementation would check that we got the full data before
-    # sending it (len(packet_in.data) should be == packet_in.total_len)).
+    def act_like_hub (self, packet, packet_in):
+        """
+        Implement hub-like behavior -- send all packets to all ports besides
+        the input port.
+        """
 
+        # We want to output to all ports -- we do that using the special
+        # OFPP_ALL port as the output port.  (We could have also used
+        # OFPP_FLOOD.)
+        self.resend_packet(packet_in, of.OFPP_ALL)
+
+        # Note that if we didn't get a valid buffer_id, a slightly better
+        # implementation would check that we got the full data before
+        # sending it (len(packet_in.data) should be == packet_in.total_len)).
 
-  def act_like_switch (self, packet, packet_in):
-    """
-    Implement switch-like behavior.
-    """
+    def act_like_switch (self, packet, packet_in):
+        """
+        Implement switch-like behavior.
+        """
 
-    # Learn the port for the source MAC
-    self.mac_to_port[packet.src] = packet_in.in_port
+        # Learn the port for the source MAC
+        self.mac_to_port[packet.src] = packet_in.in_port
 
-    # We just learned something new.  Update the switch!!
-    msg = of.ofp_flow_mod()
-    msg.match = of.ofp_match()
-    msg.match.dl_dst = packet.src
-    msg.actions.append(of.ofp_action_output(port = packet_in.in_port))
-    log.debug ("Telling switch: if dst is %s, send 'em to port %s", msg.match.dl_dst, packet_in.in_port)
-    self.connection.send(msg)
+        # We just learned something new.    Update the switch!!
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match()
+        msg.match.dl_dst = packet.src
+        msg.actions.append(of.ofp_action_output(port = packet_in.in_port))
+        log.debug ("Telling switch: if dst is %s, send 'em to port %s", msg.match.dl_dst, packet_in.in_port)
+        self.connection.send(msg)
 
-    # Now send the packet itself wherever it needs to go.
-    output_port = self.mac_to_port.get (packet.dst, of.OFPP_ALL)
+        # Now send the packet itself wherever it needs to go.
+        output_port = self.mac_to_port.get (packet.dst, of.OFPP_ALL)
     log.debug ("Packet for ether %s goes out port %s", packet.dst, output_port)
-    self.resend_packet(packet_in, output_port)
-
+        self.resend_packet(packet_in, output_port)
 
-  def _handle_PacketIn (self, event):
-    """
-    Handles packet in messages from the switch.
-    """
+    def _handle_PacketIn (self, event):
+        """
+        Handles packet in messages from the switch.
+        """
 
-    packet = event.parsed # This is the parsed packet data.
-    if not packet.parsed:
-      log.warning("Ignoring incomplete packet")
-      return
+        packet = event.parsed # This is the parsed packet data.
+        if not packet.parsed:
+            log.warning("Ignoring incomplete packet")
+            return
 
-    packet_in = event.ofp # The actual ofp_packet_in message.
+        packet_in = event.ofp # The actual ofp_packet_in message.
 
-    # Comment out the following line and uncomment the one after
-    # when starting the exercise.
-    #self.act_like_hub(packet, packet_in)
-    self.act_like_switch(packet, packet_in)
-
+        # Comment out the following line and uncomment the one after
+        # when starting the exercise.
+        #self.act_like_hub(packet, packet_in)
+        self.act_like_switch(packet, packet_in)
 
 
 def launch ():
-  """
-  Starts the component
-  """
-  def start_switch (event):
-    log.debug("Controlling %s" % (event.connection,))
-    Tutorial(event.connection)
-  core.openflow.addListenerByName("ConnectionUp", start_switch)
+    """
+    Starts the component
+    """
+    def start_switch (event):
+        log.debug("Controlling %s" % (event.connection,))
+        Tutorial(event.connection)
+    core.openflow.addListenerByName("ConnectionUp", start_switch)
